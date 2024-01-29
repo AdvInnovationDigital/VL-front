02dc82ca99d4a8f8c1fca4cf16a6bc79d5914706
Gustavo Diogo <gustavost1y@gmail.com>
2024-01-29 12:52:17 -0300

    feat: good middle

---
+++ src/documentComponents/recibohonorario/middle/beneficiario/index.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Draggable, { ControlPosition } from "react-draggable";
+
+import Person from "@/../public/vl-person.png";
+
+const MiddleCliente = (cliente: any, position: any) => {
+  const { name, profession, customerAddress, document } = cliente.cliente;
+  const { street, uf, city, district } = customerAddress;
+  const [rg, setRg] = useState<any>({});
+  const [cpf, setCpf] = useState<any>({});
+
+  const [pos, setPos] = useState<ControlPosition>({
+    x: position.x,
+    y: position.y,
+  });
+
+  useEffect(() => {
+    setCpf(
+      document.find((e: any) => {
+        return e.type == "CPF";
+      })
+    );
+    setRg(
+      document.find((e: any) => {
+        return e.type == "RG";
+      })
+    );
+  }, []);
+
+  return (
+    <Draggable bounds={"parent"} defaultPosition={pos}>
+      <div className=" w-full flex gap-10 px-10 cursor-move">
+        <div className="flex flex-col justify-center text-center w-32 items-center">
+          <Image src={Person} alt="" width={90} />
+          <span className="uppercase font-semibold">Cliente</span>
+        </div>
+        <div className="border-l-2 border-l-yellow-300" />
+        <div className="px-2 cursor-text">
+          <p>{name}</p>
+          <br />
+          <p>
+            Brasileiro (a), {profession}, portador da carteira de identidade{" "}
+            {rg.code || ""}, inscrito (a) no CPF sob o número {cpf.code || ""},
+            residente e domiciliado na {street},{district}, {city}, {uf}.
+          </p>
+        </div>
+      </div>
+    </Draggable>
+  );
+};
+
+export { MiddleCliente };
